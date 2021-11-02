b173c737da60dd97e1b3eaea905125164bdb2fe9
zxenia <ksenia1zaytseva@gmail.com>
2021-11-01 20:52:31 -0400

    organize ontologies and cv terms

---
+++ controlled_vocabularies.py
@@ -0,0 +1,75 @@
+# Controlled vocabularies for Experiments
+
+STUDY_TYPE = [
+    "Epigenomics",
+    "Proteomics",
+    "Metagenomics",
+    "Transcriptomics",
+    "Metabolomics",
+    "Other",
+]
+
+EXPERIMENT_TYPE = [
+    "DNA Methylation",
+    "RNA-Seq",
+    "mRNA-Seq",
+    "smRNA-Seq",
+    "Histone H3K4me1",
+]
+
+EXPERIMENT_ONTOLOGY = [
+    {"id": "OBI:0002118", "label": "exome sequencing assay"},
+    {"id": "OBI:0002117", "label": "whole genome sequencing assay"},
+]
+
+MOLECULE = [
+    "total RNA",
+    "polyA RNA",
+    "cytoplasmic RNA",
+    "nuclear RNA",
+    "small RNA",
+    "genomic DNA",
+    "protein",
+    "other", ]
+
+MOLECULE_ONTOLOGY = [
+    {"id": "SO:0000991", "label": "genomic DNA"},
+    {"id": "EFO:0004964", "label": "total RNA"},
+]
+
+LIBRARY_STRATEGY = [
+    "RNA-Seq",
+    "ChIP-Seq",
+    "Bisulfite-Seq",
+]
+
+LIBRARY_SOURCE = [
+    "Genomic",
+    "Genomic Single Cell",
+    "Transcriptomic",
+    "Transcriptomic Single Cell",
+    "Metagenomic",
+    "Metatranscriptomic",
+    "Synthetic",
+    "Viral RNA",
+    "Other",
+]
+
+LIBRARY_SELECTION = [
+    "Random",
+    "PCR",
+    "Random PCR",
+    "RT-PCR",
+    "MF",
+    "Other",
+]
+
+LIBRARY_LAYOUT = [
+    "Single",
+    "Paired",
+]
+
+EXTRACTION_PROTOCOL = [
+    "NGS",
+    "GBS",
+]
