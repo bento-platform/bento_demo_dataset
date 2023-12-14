ed91847d6ea5fa51d1666cda72029f39500c9d16
Gordon Krieger <gordon.krieger@gmail.com>
2023-12-14 10:18:39 -0500

    naming, numpy casts

--- random_generator/generator.py
+++ random_generator/generator.py
@@ -10,18 +10,18 @@ class RandomGenerator():
     def __init__(self):
         self.rng = np.random.default_rng(seed=RANDOM_SEED)
 
-    def choices_with_multiplicity(self, elements: list[T], multiplicity_distribution: list[float]) -> list[T]:
+    def zero_or_more_choices(self, elements: list[T], mass_distribution: list[float]) -> list[T]:
         """
         Randomly choose between zero and n items from the "elements" list
-        multiplicity_distribution is a list of weights: [P(return zero choices), P(return one choice), P(return 2 choices), etc]
+        mass_distribution is a list of weights: [P(return zero choices), P(return one choice), P(return 2 choices), etc]
         length is arbitrary, but weights should sum to one
         each individual choice is made using a normal distribution
         """
-        num_choices = self.biased_die_roll(multiplicity_distribution)
+        num_choices = self.biased_die_roll(mass_distribution)
         if num_choices == 0:
             return []
         weights_for_elements = self.gaussian_weights(len(elements))
-        return self.rng.choice(elements, size=num_choices, replace=False, p=weights_for_elements)
+        return list(self.rng.choice(elements, size=num_choices, replace=False, p=weights_for_elements))
 
     def gaussian_choice(self, elements: list[T]) -> T:
         """
@@ -36,21 +36,24 @@ class RandomGenerator():
         return self.rng.choice(len(weights), p=weights, shuffle=False)
 
     def float_from_gaussian_range(self, low: float, high: float, mean: float, sd: float) -> float:
-        return truncnorm.rvs((low-mean)/sd, (high-mean)/sd, loc=mean, scale=sd, random_state=self.rng) 
+        return float(truncnorm.rvs((low-mean)/sd, (high-mean)/sd, loc=mean, scale=sd, random_state=self.rng))
 
     def int_from_gaussian_range(self, low: float, high: float, mean: float, sd: float) -> int:
         return floor(self.float_from_gaussian_range(low, high, mean, sd))
 
     def float_from_exponential_range(self, low: float, high: float, mean: float) -> float:
-        return truncexpon.rvs((high - low)/mean, loc=low, scale=mean, random_state=self.rng)
+        return float(truncexpon.rvs((high - low)/mean, loc=low, scale=mean, random_state=self.rng))
 
     def int_from_exponential_range(self, low: float, high: float, mean: float) -> int:
         return floor(self.float_from_exponential_range(low, high, mean))
 
-    def gaussian_weights(self, size: int) -> list[float]:
+    def int_from_uniform_range(self, low, high) -> int:
+        return int(self.rng.integers(low, high))
+
+    def gaussian_weights(self, size: int) -> list[np.float64]:
         """
         weights for choice methods
-        These are only quasi-gaussian since we take the absoute value and normalize so they sum to one
+        These are only quasi-gaussian since we take the absolute value and normalize so they sum to one
         """
         g = self.rng.normal(size=size)
         g_pos = abs(g)
@@ -59,6 +62,7 @@ class RandomGenerator():
     
     def random_bytes(self, n: int) -> bytes:
         return self.rng.bytes(n)
+
 
     # TODO
     # def random_weights(self, controlled_vocabulary):
