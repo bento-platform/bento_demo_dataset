909719f49cf01e25f438f71a190a8315a96d481a
Gordon Krieger <gordon.krieger@gmail.com>
2024-01-16 16:04:23 -0500

    add some medical actions

---
+++ phenopackets/medical_actions.py
@@ -0,0 +1,68 @@
+TREATMENTS = [
+    {
+        "agent": {
+            "id": "Ondansetron",
+            "label": "NCIT:C1119"
+        }
+    },
+    {
+        "agent": {
+            "id": "NCIT:C198",
+            "label": "Acetaminophen"
+        },
+        "routeOfAdministration": {
+            "id": "NCIT:C38288",
+            "label": "Oral Route of Administration"
+        },
+        "doseIntervals": [
+            {
+                "quantity": {
+                    "unit": {
+                        "id": "UO:0000022",
+                        "label": "milligram"
+                    },
+                    "value": 500
+                },
+                "scheduleFrequency": {
+                    "id": "NCIT:C64496",
+                    "label": "Twice Daily"
+                },
+                "interval": {
+                    "start": "xxxxxxxxxxx",
+                    "end": "xxxxxxxxxxxxxxx"
+                }
+            }
+        ],
+        "drugType": "PRESCRIPTION"
+    },
+    {
+        "agent": {
+            "id": "NCIT:C561",
+            "label": "Ibuprofen"
+        },
+        "routeOfAdministration": {
+            "id": "NCIT:C38288",
+            "label": "Oral Route of Administration"
+        },
+        "doseIntervals": [
+            {
+                "quantity": {
+                    "unit": {
+                        "id": "UO:0000022",
+                        "label": "milligram"
+                    },
+                    "value": 500
+                },
+                "scheduleFrequency": {
+                    "id": "NCIT:C64496",
+                    "label": "Twice Daily"
+                },
+                "interval": {
+                    "start": "xxxxxxxxxxx",
+                    "end": "xxxxxxxxxxxxxxx"
+                }
+            }
+        ],
+        "drugType": "PRESCRIPTION"
+    }
+]
