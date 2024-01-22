dc822ba933958769711ada114c53abfa7c0338fd
Gordon Krieger <gordon.krieger@gmail.com>
2024-01-22 10:28:52 -0500

    add diseases with stages

---
+++ phenopackets/diseases.py
@@ -0,0 +1,74 @@
+DISEASES = [
+    {
+        "term": {
+            "id": "MONDO:0005015",
+            "label": "diabetes mellitus"
+        }
+    },
+    {
+        "term": {
+            "id": "MONDO:0012011",
+            "label": "coronary artery disease, autosomal dominant, 1"
+        }
+    },
+    {
+        "term": {
+            "id": "MONDO:0000603",
+            "label": "autoimmune disorder of cardiovascular system"
+        }
+    },
+    {
+        "term": {
+            "id": "MONDO:0006012",
+            "label": "viral pneumonia"
+        }
+    },
+    {
+        "term": {
+            "id": "SNOMED:85828009",
+            "label": "Rheumatologic disease"
+        }
+    },
+    {
+        "term": {
+            "id": "SNOMED:13645005",
+            "label": "Chronic obstructive lung disease"
+        }
+    },
+    {
+        "term": {
+            "id": "NCIT:C27191",
+            "label": "Hashimoto Thyroiditis"
+        }
+    },
+    {
+        "term": {
+            "id": "SNOMED:109992005",
+            "label": "Polycythemia vera"
+        }
+    },
+    {
+        "term": {
+            "id": "NCIT:C3058",
+            "label": "Glioblastoma"
+        },
+        "disease_stage": {
+            "id": "NCIT:C27971",
+            "label": "Stage IV"
+        },
+        "clinical_tnm_finding": {
+            "id": "NCIT:C161009",
+            "label": "Clinical Primary Tumor TNM Finding"
+        }
+    },
+    {
+        "term": {
+            "id": "NCIT:C3211",
+            "label": "Non-Hodgkin Lymphoma"
+        },
+        "disease_stage": {
+            "id": "NCIT:C141199",
+            "label": "Stage IE"
+        },
+    },
+]
