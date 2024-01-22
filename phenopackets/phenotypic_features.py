1f772e12a74dc136e63f5312ffc6212de8ffd436
Gordon Krieger <gordon.krieger@gmail.com>
2024-01-22 10:31:11 -0500

    add phenotypic features

---
+++ phenopackets/phenotypic_features.py
@@ -0,0 +1,210 @@
+def phenotypic_features():
+    return [{"type": p for p in PHENOTYPIC_FEATURES}]
+
+
+PHENOTYPIC_FEATURES = [
+    {
+        "id": "HP:0000822",
+        "label": "Hypertension"
+    },
+    {
+        "id": "SNOMED:84114007",
+        "label": "Heart failure"
+    },
+    {
+        "id": "SNOMED:62315008",
+        "label": "Diarrhea"
+    },
+    {
+        "id": "SNOMED:29857009",
+        "label": "Chest pain"
+    },
+    {
+        "id": "SNOMED:267036007",
+        "label": "Dyspnea"
+    },
+    {
+        "id": "SNOMED:386661006",
+        "label": "Fever"
+    },
+    {
+        "id": "SNOMED:267102003",
+        "label": "Sore throat symptom"
+    },
+    {
+        "id": "SNOMED:49727002",
+        "label": "Cough"
+    },
+    {
+        "id": "SNOMED:52448006",
+        "label": "Dementia"
+    },
+    {
+        "id": "SNOMED:79890006",
+        "label": "Loss of appetite"
+    },
+    {
+        "id": "SNOMED:195967001",
+        "label": "Asthma"
+    },
+    {
+        "id": "SNOMED:75570004",
+        "label": "Viral pneumonia/pneumonitis"
+    },
+    {
+        "id": "SNOMED:84229001",
+        "label": "Fatigue"
+    },
+    {
+        "id": "SNOMED:25064002",
+        "label": "Headache"
+    },
+    {
+        "id": "SNOMED:68962001",
+        "label": "Muscle pain"
+    },
+    {
+        "id": "SNOMED:44169009",
+        "label": "Loss of sense of smell"
+    },
+    {
+        "id": "SNOMED:370391006",
+        "label": "Patient immunosuppressed"
+    },
+    {
+        "id": "SNOMED:414916001",
+        "label": "Obesity"
+    },
+    {
+        "id": "SNOMED:422587007",
+        "label": "Nausea"
+    },
+    {
+        "id": "SNOMED:64531003",
+        "label": "Nasal discharge"
+    },
+    {
+        "id": "SNOMED:49436004",
+        "label": "New atrial fibrillation or flutter (AF)"
+    },
+    {
+        "id": "SNOMED:14350001000004108",
+        "label": "Acute injury of kidney"
+    },
+    {
+        "id": "NCIT:C50634",
+        "label": "Liver Dysfunction"
+    },
+    {
+        "id": "SNOMED:21522001",
+        "label": "Abdominal pain"
+    },
+    {
+        "id": "SNOMED:266257000",
+        "label": "Prior TIA"
+    },
+    {
+        "id": "SNOMED:56018004",
+        "label": "Wheezing"
+    },
+    {
+        "id": "SNOMED:87486003",
+        "label": "Aphasia"
+    },
+    {
+        "id": "SNOMED:80394007",
+        "label": "Hyperglycemia"
+    },
+    {
+        "id": "SNOMED:404640003",
+        "label": "Dizziness"
+    },
+    {
+        "id": "SNOMED:66857006",
+        "label": "Hemoptysis"
+    },
+    {
+        "id": "SNOMED:70995007",
+        "label": "Pulmonary hypertension"
+    },
+    {
+        "id": "SNOMED:410429000",
+        "label": "Cardiac arrest"
+    },
+    {
+        "id": "SNOMED:233604007",
+        "label": "Bacteria pneumonia"
+    },
+    {
+        "id": "SNOMED:56819008",
+        "label": "Endocarditis"
+    },
+    {
+        "id": "SNOMED:36118008",
+        "label": "Pneumothorax"
+    },
+    {
+        "id": "SNOMED:9826008",
+        "label": "Conjunctivitis"
+    },
+    {
+        "id": "SNOMED:16001004",
+        "label": "Ear pain"
+    },
+    {
+        "id": "SNOMED:128053003",
+        "label": "Deep vein thrombosis (DVT)"
+    },
+    {
+        "id": "SNOMED:25569003",
+        "label": "Ventricular tachycardia"
+    },
+    {
+        "id": "SNOMED:2492009",
+        "label": "Malnutrition"
+    },
+    {
+        "id": "SNOMED:271737000",
+        "label": "Anemia"
+    },
+    {
+        "id": "SNOMED:75694006",
+        "label": "Pancreatitis"
+    },
+    {
+        "id": "SNOMED:424404003",
+        "label": "Decompensated heart failure"
+    },
+    {
+        "id": "SNOMED:302866003",
+        "label": "Hypoglycemia"
+    },
+    {
+        "id": "NCIT:C3108",
+        "label": "HIV Infection"
+    },
+    {
+        "id": "SNOMED:91175000",
+        "label": "Seizure"
+    },
+    {
+        "id": "SNOMED:50920009",
+        "label": "Myocarditis"
+    },
+    {
+        "id": "SNOMED:240131006",
+        "label": "Rhabdomyolysis"
+    },
+    {
+        "id": "SNOMED:74474003",
+        "label": "Gastrointestinal haemorrhage"
+    },
+    {
+        "id": "SNOMED:7180009",
+        "label": "Meningitis"
+    },
+    {
+        "id": "SNOMED:3238004",
+        "label": "Pericarditis"
+    },
+]
