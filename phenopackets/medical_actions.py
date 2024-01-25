1e50f8186c3c01870a8815d7a882f727ac0388f3
Gordon Krieger <gordon.krieger@gmail.com>
2024-01-25 16:43:40 -0500

    parameterize treatments

--- phenopackets/medical_actions.py
+++ phenopackets/medical_actions.py
@@ -35,71 +35,75 @@ PROCEDURES = [
     }
 ]
 
-TREATMENTS = [
-    {
-        "agent": {
-            "id": "Ondansetron",
-            "label": "NCIT:C1119"
+
+# TODO? treatment dates are "recent" but may be before birth :(
+def treatments(rng):
+    interval = rng.recent_interval_start_and_end_datetime_strings(max_days=14)
+    return [
+        {
+            "agent": {
+                "id": "Ondansetron",
+                "label": "NCIT:C1119"
+            }
+        },
+        {
+            "agent": {
+                "id": "NCIT:C198",
+                "label": "Acetaminophen"
+            },
+            "routeOfAdministration": {
+                "id": "NCIT:C38288",
+                "label": "Oral Route of Administration"
+            },
+            "doseIntervals": [
+                {
+                    "quantity": {
+                        "unit": {
+                            "id": "UO:0000022",
+                            "label": "milligram"
+                        },
+                        "value": 500
+                    },
+                    "scheduleFrequency": {
+                        "id": "NCIT:C64496",
+                        "label": "Twice Daily"
+                    },
+                    "interval": {
+                        "start": interval["start"],
+                        "end": interval["end"]
+                    }
+                }
+            ],
+            "drugType": "PRESCRIPTION"
+        },
+        {
+            "agent": {
+                "id": "NCIT:C561",
+                "label": "Ibuprofen"
+            },
+            "routeOfAdministration": {
+                "id": "NCIT:C38288",
+                "label": "Oral Route of Administration"
+            },
+            "doseIntervals": [
+                {
+                    "quantity": {
+                        "unit": {
+                            "id": "UO:0000022",
+                            "label": "milligram"
+                        },
+                        "value": 500
+                    },
+                    "scheduleFrequency": {
+                        "id": "NCIT:C64496",
+                        "label": "Twice Daily"
+                    },
+                    "interval": {
+                        "start": interval["start"],
+                        "end": interval["end"]
+                    }
+                }
+            ],
+            "drugType": "PRESCRIPTION"
         }
-    },
-    {
-        "agent": {
-            "id": "NCIT:C198",
-            "label": "Acetaminophen"
-        },
-        "routeOfAdministration": {
-            "id": "NCIT:C38288",
-            "label": "Oral Route of Administration"
-        },
-        "doseIntervals": [
-            {
-                "quantity": {
-                    "unit": {
-                        "id": "UO:0000022",
-                        "label": "milligram"
-                    },
-                    "value": 500
-                },
-                "scheduleFrequency": {
-                    "id": "NCIT:C64496",
-                    "label": "Twice Daily"
-                },
-                "interval": {
-                    "start": "xxxxxxxxxxx",
-                    "end": "xxxxxxxxxxxxxxx"
-                }
-            }
-        ],
-        "drugType": "PRESCRIPTION"
-    },
-    {
-        "agent": {
-            "id": "NCIT:C561",
-            "label": "Ibuprofen"
-        },
-        "routeOfAdministration": {
-            "id": "NCIT:C38288",
-            "label": "Oral Route of Administration"
-        },
-        "doseIntervals": [
-            {
-                "quantity": {
-                    "unit": {
-                        "id": "UO:0000022",
-                        "label": "milligram"
-                    },
-                    "value": 500
-                },
-                "scheduleFrequency": {
-                    "id": "NCIT:C64496",
-                    "label": "Twice Daily"
-                },
-                "interval": {
-                    "start": "xxxxxxxxxxx",
-                    "end": "xxxxxxxxxxxxxxx"
-                }
-            }
-        ],
-        "drugType": "PRESCRIPTION"
-    }
-]
+    ]
