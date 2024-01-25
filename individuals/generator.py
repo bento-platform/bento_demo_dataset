ca0dbae4ecb4b290b26baa2a234046ed1ac20629
Gordon Krieger <gordon.krieger@gmail.com>
2024-01-25 16:49:49 -0500

    individual generator improvements

--- individuals/generator.py
+++ individuals/generator.py
@@ -1,57 +1,70 @@
 import numpy as np
 from constants import (
     AGE_MEAN, AGE_SD, AGE_MIN, AGE_MAX, DISEASE_MASS_DISTRIBUTION, PHENOTYPIC_FEATURE_MASS_DISTRIBUTION,
-    LAB_MIN, LAB_MAX, LAB_MEAN, P_EXCLUDED, BMI_MIN, BMI_MAX, P_BP_PRESENT, BP_MIN, BP_MAX, BP_MEAN,
-    BP_SD, BMI_MEAN, BMI_SD, P_BMI_PRESENT, P_SMOKING_STATUS_PRESENT)
+    LAB_MIN, LAB_MAX, LAB_MEAN, P_EXCLUDED, P_SMOKING_STATUS_PRESENT, MEDICAL_ACTION_MASS_DISTRIBUTION,
+    INTERPRETATION_MASS_DISTRIBUTION, EXTRA_BIOSAMPLES_MASS_DISTRIBUTION, P_ADD_EXPERIMENT_TO_BIOSAMPLE,
+    P_ADD_EXAMPLE_FILE_TO_EXPERIMENT)
+from experiments.experiment_metadata import one_thousand_genomes_experiment, random_synthetic_experiment
 from phenopackets.extra_properties import SMOKING_STATUS, COVID_SEVERITY, MOBILITY
-from ontology_terms import DISEASE_TERMS, PHENOTYPIC_FEATURES, DISEASE_STAGES, TNM_FINDING
+from phenopackets.diseases import DISEASES, COVID_19
+from phenopackets.interpretations import interpretations
+from phenopackets.measurements import has_bmi, has_blood_pressure, bmi, blood_pressure
+from phenopackets.medical_actions import PROCEDURES, treatments
+from phenopackets.metadata import metadata
+from phenopackets.phenotypic_features import phenotypic_features
 from random_generator.generator import RandomGenerator
-from experiments.experiment_metadata import one_thousand_genomes_experiment, EXPERIMENT_RESOURCES
 
 
 ##############################
 # conventions:
-# generate one phenopacket per individual
+# generate one phenopacket per individual in individuals.json
 #
 # for 1000 genomes ids only:
 # - create a biosample and experiment with reference to a vcf file (file may or may not exist)
 # - biosample_id = individual_id
 # - vcf file name is <individual_id>.vcf.gz
 #
-# for other individuals:
-# - copy over any experiments and experiment results in "experiments" field
+# for any ids, copy over the value of these fields where they exist:
+# - biosamples
+# - experiments
+# - diseases
 #
-
-
-
-
-# why are our phenopackets in snake_case??
-# distribute a few alternate ids (lists of ids, although pxf docs say list of CURIE ??)
-
+# ... this allows us to have sensible values associated with any real data files we use,
+# eg: we can make sure that biosample ids match between a real file and imaginary metadata
+# or cancer data can have cancer mentioned in the metadata
 
 
 class Individual:
     def __init__(self, rng, individual):
         self.rng: RandomGenerator = rng
-        self.individual: dict = individual
+        self.individual = individual
         self.experiments = individual.get("experiments", [])
         self.phenopacket = self.generate_phenopacket()
 
     def generate_phenopacket(self):
         p = {
-            "id": self.rng.random_uuid4(),
+            "id": self.rng.uuid4(),
+            # "alternate_ids": []  # why are alternate ids CURIEs?
             "subject": self.subject(),
             "biosamples": self.biosamples(),
-            "meta_data": self.phenopacket_metadata()
+            "phenotypic_features": self.phenotypic_features()
         }
 
-        # conditional additions
-        measurements = self.measurements()
-        if measurements:
-            p["measurements"] = measurements
+        # --------- conditional additions ------------
+        if ms := self.measurements():
+            p["measurements"] = ms
 
-        # TODO: medical actions
-        # TODO: interpretations
+        if ma := self.medical_actions():
+            p["medical_actions"] = ma
+
+        if intp := self.interpretations():
+            p["interpretations"] = intp
+        # --------------------------------------------
+            
+        # random diseases, plus diseases mentioned elsewhere in this phenopacket
+        p["diseases"] = self.diseases(intp, p["subject"]["extra_properties"]["covid_severity"])
+
+        p["meta_data"] = self.metadata()
 
         return p
 
@@ -77,18 +90,18 @@ class Individual:
             "extra_properties": {
                 "mobility": self.rng.gaussian_choice(MOBILITY),
                 "covid_severity": self.rng.gaussian_choice(COVID_SEVERITY),
-                "date_of_consent": self.rng.random_recent_date(),
+                "date_of_consent": self.rng.recent_date_string(),
                 "lab_test_result_value": self.lab_value()
             }
         }
 
         # conditionally add smoking status extra property
-        smoking = self.smoking_status()
-        if smoking:
-            s["extra_properties"]["smoking_status"] = smoking
+        if self.has_smoking_status():
+            s["extra_properties"]["smoking_status"] = self.smoking_status()
 
         return s
 
+    # creates experiments associated with a biosample as a side effect
     def biosamples(self):
         # return any real stuff from config
         if bs := self.individual.get("biosamples"):
@@ -96,7 +109,7 @@ class Individual:
 
         b = []
 
-        # add an experiment with vcf for all 1000 genomes samples, whether vcf exists or not
+        # add an experiment with vcf for all 1000 genomes ids, whether vcf exists or not
         if self.has_1000_genomes_sample():
             b.append({
                 "id": self.individual["id"],  # convention for 1k genomes biosample ids
@@ -107,77 +120,78 @@ class Individual:
             })
             self.add_experiment(one_thousand_genomes_experiment(self.rng, self.individual["id"]))
 
-        # chose zero or more fake biosamples
-        # either with zero_or_more_choices on a list of biosamples fns
-        # or die roll, then that many calls to random biosample generator
-        # even if the second way, some fields will need to be set?
+        # randomly add more biosamples...
+        extra_biosamples = self.rng.zero_or_more_choices(
+            [f"{self.individual['id']}-{n}" for n in range(len(EXTRA_BIOSAMPLES_MASS_DISTRIBUTION))],
+            EXTRA_BIOSAMPLES_MASS_DISTRIBUTION)
 
-        # required field:
-        # id
-        # recommended fields:
-        # individual_id
-        # phenotypic_features (of sample)
-        # time_of_collection
-        # histological_diagnosis
-        # tumor_progression
-        # pathological_stage
-        # pathological_tnm_finding
-        # diagnostic_markers
-        # procedure
-        # material_sample
+        # ... then typically give them experiments
+        for eb_id in extra_biosamples:
+            if self.should_add_experiment_to_biosample():
+                self.add_experiment(random_synthetic_experiment(self.rng, eb_id))
 
-        # randomly choose between zero and n biosamples
-        # but at least make them coherent
-
+        
+        # TODO?
+        # could have more top-level biosample properties (procedure, etc)
+        # but some properties only make sense with particular experiments
+        
         return b
 
-    def diseases(self):
-        # return anything in config
-        if ds := self.individual.get("diseases"):
-            return ds
-
+    def diseases(self, intp, covid_severity):
         # randomly choose between zero and n diseases
-        ds = self.rng.zero_or_more_choices(DISEASE_TERMS, DISEASE_MASS_DISTRIBUTION)
+        ds = self.rng.zero_or_more_choices(DISEASES, DISEASE_MASS_DISTRIBUTION)
 
         # very rarely, mark a disease as excluded
-        ds_ex = [{**d, "excluded": True} if self.rng.biased_coin_toss(P_EXCLUDED) else d for d in ds]
+        ds_ex = [
+            {**d, "excluded": True}
+            if self.rng.biased_coin_toss(P_EXCLUDED) and "disease_stage" not in d else d for d in ds]
+
+        # add anything in config
+        if config_ds := self.individual.get("diseases"):
+            ds_ex.extend(config_ds)
+
+        # add any diseases mentioned in "interpretations"
+        ds_ex.extend({"term": i["diagnosis"]["disease"]} for i in intp if "diagnosis" in i)
+
+        # add covid if present
+        if covid_severity != "Uninfected":
+            ds_ex.append(COVID_19)
+
         return ds_ex
 
     def phenotypic_features(self):
         # randomly choose between zero and n phenotypic features
-        pfs = self.rng.zero_or_more_choices(PHENOTYPIC_FEATURES, PHENOTYPIC_FEATURE_MASS_DISTRIBUTION)
+        pfs = self.rng.zero_or_more_choices(phenotypic_features(), PHENOTYPIC_FEATURE_MASS_DISTRIBUTION)
 
         # very rarely, mark as excluded
         pfs_ex = [{**p, "excluded": True} if self.rng.biased_coin_toss(P_EXCLUDED) else p for p in pfs]
         return pfs_ex
 
-    # conditionally add a measurement
     def measurements(self):
         ms = []
-        if self.has_bmi():
-            ms.append(self.bmi())
+        if has_bmi(self.rng):
+            ms.append(bmi(self.rng))
 
-        if self.has_blood_pressure():
-            ms.append(self.blood_pressure())
+        if has_blood_pressure(self.rng):
+            ms.append(blood_pressure(self.rng))
 
         return ms
 
-    # TODO
     def interpretations(self):
-        ...
+        return self.rng.zero_or_more_choices(interpretations(self.individual["id"]), INTERPRETATION_MASS_DISTRIBUTION)
 
-    # TODO
     def medical_actions(self):
-        ...
+        action_procedures = self.rng.zero_or_more_choices(PROCEDURES, MEDICAL_ACTION_MASS_DISTRIBUTION)
+        action_treatments = self.rng.zero_or_more_choices(treatments(self.rng), MEDICAL_ACTION_MASS_DISTRIBUTION)
+        # could add top-level values here (treatment_target, etc)
+        return action_procedures + action_treatments
 
     # possible later TODO, currently we only attach files to experiments, not to phenopackets
     def files(self):
-        ...
+        pass
 
-    # TODO: only add metadata actually used for this individual?
-    #  or simply shovel all into everyone
-    def meta_data(self):
-        ...
+    def metadata(self):
+        return metadata()
 
 # utils ------------------------
 
@@ -192,66 +206,6 @@ class Individual:
 
     def smoking_status(self) -> list[str]:
         return self.rng.gaussian_choice(SMOKING_STATUS)
-
-    def has_bmi(self) -> bool:
-        return bool(self.rng.biased_coin_toss(P_BMI_PRESENT))
-
-    def bmi(self):
-        return {
-            "assay": {"id": "NCIT:C16358", "label": "Body Mass Index"},
-            "measurement_value": {
-                "unit": {"id": "NCIT:C49671", "label": "Kilogram per Square Meter"},
-                "value": round(self.rng.float_from_gaussian_range(BMI_MIN, BMI_MAX, BMI_MEAN, BMI_SD), 2)
-            }
-        }
-
-    def has_blood_pressure(self) -> bool:
-        return bool(self.rng.biased_coin_toss(P_BP_PRESENT))
-
-    def blood_pressure(self):
-        random_offset_range = 10, 20
-        mean_bp = self.rng.int_from_gaussian_range(BP_MIN, BP_MAX, BP_MEAN, BP_SD)
-        offset = self.rng.int_from_uniform_range(random_offset_range[0], random_offset_range[1])
-        diastolic, systolic = mean_bp - offset, mean_bp + offset
-        return {
-            "assay": {"id": "NCIT:C167233", "label": "Blood Pressure Measurement"},
-            "complexValue": {
-                "typedQuantities": [
-                    {
-                        "type": {
-                            "id": "NCIT:C25298", "label": "Systolic Blood Pressure"
-                        },
-                        "quantity": {
-                            "unit": {"id": "NCIT:C49670", "label": "Millimeter of Mercury"},
-                            "value": systolic
-                        }
-                    },
-                    {
-                        "type": {"id": "NCIT:C25299", "label": "Diastolic Blood Pressure"},
-                        "quantity": {
-                            "unit": {"id": "NCIT:C49670", "label": "Millimeter of Mercury"},
-                            "value": diastolic
-                        }
-                    }
-                ]
-            }
-        }
-
-    # ideally we would only mention resources used in this phenopacket
-    # TODO: needs correct resources
-    def phenopacket_metadata(self):
-        return {
-            "created": self.rng.random_recent_date(),
-            "created_by": "C3G_synthetic_data",
-            "phenopacket_schema_version": "2.0.0",
-            "resources": [
-                {
-                    "name": "Sequence types and features ontology",
-                    "version": "2021-02-16",
-                    "namespace_prefix": "SO",
-                    "id": "SO:2021-02-16",
-                    "iri_prefix": "http://purl.obolibrary.org/obo/so.owl#",
-                    "url": "http://purl.obolibrary.org/obo/so.owl"
-                }
-            ]
-        }
+    
+    def should_add_experiment_to_biosample(self):
+        return bool(self.rng.biased_coin_toss(P_ADD_EXPERIMENT_TO_BIOSAMPLE))
