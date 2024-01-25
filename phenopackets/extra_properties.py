2b9d54562d8eda66c8a8c95bd6eceabd0b404cf6
Gordon Krieger <gordon.krieger@gmail.com>
2024-01-25 16:47:26 -0500

    categories for subject extra properties

---
+++ phenopackets/extra_properties.py
@@ -0,0 +1,24 @@
+# values for catgegorical extra properties
+SMOKING_STATUS = [
+    "Non-smoker",
+    "Smoker",
+    "Former smoker",
+    "Passive smoker",
+    "Not specified"
+]
+
+MOBILITY = [
+    "I have no problems in walking about",
+    "I have slight problems in walking about",
+    "I have moderate problems in walking about",
+    "I have severe problems in walking about",
+    "I am unable to walk about"
+]
+
+COVID_SEVERITY = [
+    "Uninfected",
+    "Mild",
+    "Moderate",
+    "Severe",
+    "Dead"
+]
