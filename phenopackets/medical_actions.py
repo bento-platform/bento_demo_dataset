0b724df0a0e8dc68baf32808c8cec607c9c0fdbe
Gordon Krieger <gordon.krieger@gmail.com>
2024-01-29 13:46:34 -0500

    medical action fixes

--- phenopackets/medical_actions.py
+++ phenopackets/medical_actions.py
@@ -1,36 +1,46 @@
 PROCEDURES = [
     {
-        "code": {
-            "id": "NCIT:C28743",
-            "label": "Punch Biopsy"
+        "procedure": {
+            "code": {
+                "id": "NCIT:C28743",
+                "label": "Punch Biopsy"
+            }
         }
     },
     {
-        "code": {
-            "id": "NCIT:C16809",
-            "label": "Magnetic Resonance Imaging"
+        "procedure": {
+            "code": {
+                "id": "NCIT:C16809",
+                "label": "Magnetic Resonance Imaging"
+            }
         }
     },
     {
-        "code": {
-            "id": "NCIT:C38101",
-            "label": "X-Ray Imaging"
+        "procedure": {
+            "code": {
+                "id": "NCIT:C38101",
+                "label": "X-Ray Imaging"
+            }
         }
     },
     {
-        "code": {
-            "id": "NCIT:C17007",
-            "label": "Positron Emission Tomography"
+        "procedure": {
+            "code": {
+                "id": "NCIT:C17007",
+                "label": "Positron Emission Tomography"
+            }
         }
     },
     {
-        "code": {
-            "id": "NCIT:C51677",
-            "label": "Liver Biopsy"
-        },
-        "body_site": {
-            "id": "UBERON:0001115",
-            "label": "left lobe of liver"
+        "procedure": {
+            "code": {
+                "id": "NCIT:C51677",
+                "label": "Liver Biopsy"
+            },
+            "body_site": {
+                "id": "UBERON:0001115",
+                "label": "left lobe of liver"
+            }
         }
     }
 ]
@@ -40,22 +50,23 @@ PROCEDURES = [
 def treatments(rng):
     interval = rng.recent_interval_start_and_end_datetime_strings(max_days=14)
     return [
-        {
+        {"treatment": {
             "agent": {
                 "id": "Ondansetron",
                 "label": "NCIT:C1119"
             }
+        }
         },
-        {
+        {"treatment": {
             "agent": {
                 "id": "NCIT:C198",
                 "label": "Acetaminophen"
             },
-            "routeOfAdministration": {
+            "route_of_administration": {
                 "id": "NCIT:C38288",
                 "label": "Oral Route of Administration"
             },
-            "doseIntervals": [
+            "dose_intervals": [
                 {
                     "quantity": {
                         "unit": {
@@ -64,7 +75,7 @@ def treatments(rng):
                         },
                         "value": 500
                     },
-                    "scheduleFrequency": {
+                    "schedule_frequency": {
                         "id": "NCIT:C64496",
                         "label": "Twice Daily"
                     },
@@ -74,18 +85,18 @@ def treatments(rng):
                     }
                 }
             ],
-            "drugType": "PRESCRIPTION"
-        },
-        {
+            "drug_type": "PRESCRIPTION"
+        }},
+        {"treatment": {
             "agent": {
                 "id": "NCIT:C561",
                 "label": "Ibuprofen"
             },
-            "routeOfAdministration": {
+            "route_of_administration": {
                 "id": "NCIT:C38288",
                 "label": "Oral Route of Administration"
             },
-            "doseIntervals": [
+            "dose_intervals": [
                 {
                     "quantity": {
                         "unit": {
@@ -94,7 +105,7 @@ def treatments(rng):
                         },
                         "value": 500
                     },
-                    "scheduleFrequency": {
+                    "schedule_frequency": {
                         "id": "NCIT:C64496",
                         "label": "Twice Daily"
                     },
@@ -104,6 +115,6 @@ def treatments(rng):
                     }
                 }
             ],
-            "drugType": "PRESCRIPTION"
-        }
+            "drug_type": "PRESCRIPTION"
+        }}
     ]
