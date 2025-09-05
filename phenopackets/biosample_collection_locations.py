25e645ae992929ab3fd533e51afa006ad85219ff
Gordon Krieger <gordon.krieger@gmail.com>
2025-09-05 15:39:37 -0400

    add location_collected property to some biosamples

---
+++ phenopackets/biosample_collection_locations.py
@@ -0,0 +1,37 @@
+# see here for guidelines on coordinate precision: https://xkcd.com/2170/
+
+BIOSAMPLE_LOCATIONS = [
+    {
+        "type": "Feature",
+        "geometry": {"type": "Point", "coordinates": [-73.6034936, 45.4732202, 47]},
+        "properties": {"label": "MUHC"},
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-73.5674, 45.5019],
+        },
+        "properties": {
+            "label": "Montreal",
+            "city": "Montreal",
+            "country": "Canada",
+            "ISO3166alpha3": "CAN",
+            "precision": "city",
+            "location_extra_property": 4321,
+        },
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-73.6251, 45.5031],
+        },
+        "properties": {
+            "label": "CHU Sainte-Justine",
+            "city": "Montreal",
+            "country": "Canada",
+            "ISO3166alpha3": "CAN",
+        },
+    },
+]
