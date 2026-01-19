58b3d5943bf0043b75dddca714eef900e883bdb7
David Lougheed <david.lougheed@gmail.com>
2026-01-19 15:24:51 -0500

    more work on vital status

--- individuals/vital_status.py
+++ individuals/vital_status.py
@@ -1,31 +1,32 @@
 VITAL_STATUS_ENUM = ["UNKNOWN_STATUS", "ALIVE", "DECEASED"]
 VITAL_STATUS_CAUSES_OF_DEATH = [
     {
-        "id": "SNOMED:217275006",
-        "label": "Accident due to fall from burning travel trailer (event)",
-    },
-    {
         "id": "SNOMED:1428003",
-        "label": "Asphyxia due to foreign body in larynx (event)",
+        "label": "Asphyxia due to foreign body in larynx",
     },
     {
-        "id": "SNOMED:217442000",
-        "label": "Hit by object falling from burning church (event)",
+        "id": "SNOMED:230690007",
+        "label": "Cerebrovascular accident",
     },
     {
         "id": "SNOMED:58908002",
-        "label": "Exposure to attack by amphibian (event)",
+        "label": "Exposure to attack by amphibian",
     },
     {
-        "id": "SNOMED:58538004",
-        "label": "Bumped into other person (event)",
+        "id": "SNOMED:768147005",
+        "label": "Injury due to car accident",
     },
     {
-        "id": "SNOMED:111942003",
-        "label": "Legal execution by lethal injection (event)",
+        "id": "SNOMED:22298006",
+        "label": "Myocardial infarction",
     },
     {
         "id": "NCIT:C36263",
         "label": "Metastatic Malignant Neoplasm",
     },
 ]
+VITAL_STATUS_CAUSE_OF_DEATH_COVID_19 = {
+    "id": "SNOMED:1119302008",
+    # technically the wrong label, "severe acute respiratory syndrome coronavirus 2" is very long.
+    "label": "Acute disease caused by SARS-CoV-2",
+}
